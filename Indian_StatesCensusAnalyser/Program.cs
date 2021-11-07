0c30b7fd07d1c77d35c27d53224d5ab7feeb5b14
NoorMdKhan123 <noormd6400@gmail.com>
2021-11-07 14:27:04 +0530

    Indian state analyser program

--- Indian_StatesCensusAnalyser/Program.cs
+++ Indian_StatesCensusAnalyser/Program.cs
@@ -6,7 +6,7 @@ namespace Indian_StatesCensusAnalyser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Indian census analyser program");
         }
     }
 }
